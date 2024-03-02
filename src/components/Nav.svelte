515e9ae5ac221a4adfb8336b751a1f27ff29ff67
Swas.py <codewithswastik@gmail.com>
2024-03-02 16:21:44 +0530

    feat: Add icons and Video component

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,9 @@
 		<h1 class="text-white text-2xl">BITSmegle</h1>
 	</div>
 	<div class="h-full flex flex-row items-center justify-between">
-		<div class="py-1 px-2.5 bg-emerald-900 text-emerald-400 rounded-lg mr-4">U 3</div>
+		<div class="py-1 px-2.5 bg-emerald-900 text-emerald-400 rounded-lg mr-4">
+			<i class="fas fa-user text-sm"></i> <span>3</span>
+		</div>
 		<img
 			class="h-[50%] rounded-full"
 			src="https://superapk.one/wp-content/uploads/2022/10/Omegle-1.png"
---
+++ src/components/Video.svelte
@@ -0,0 +1,12 @@
+<div class="relative h-[90%] w-[48%] rounded-3xl overflow-hidden">
+	<div
+		class="backdrop-blur-md absolute flex flex-col rounded-lg left-0 bottom-0 z-10 m-5 px-3 py-2"
+	>
+		<h2 class="text-xl text-gray-200">User Name</h2>
+		<h2 class="text-sm text-gray-300">2023A7PS0000H</h2>
+	</div>
+	<img
+		class="h-full w-full object-cover obje"
+		src="https://i.pinimg.com/originals/55/de/7a/55de7a6bb9e994d30d132e9d4b154855.jpg"
+	/>
+</div>
