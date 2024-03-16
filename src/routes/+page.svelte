bcc47487e318d7d1c46fa95d7fde4dba299bee29
Swas.py <codewithswastik@gmail.com>
2024-03-17 00:44:00 +0530

    feat: Fix landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,7 @@
 
 	onMount(() => {
 		// Calculate the time remaining until 7 days from now
-		const deadline = new Date();
-		deadline.setDate(20);
+		const deadline = new Date(2024, 2, 20, 22, 0, 0);
 
 		// Function to update the countdown timer
 		function updateCountdown() {
@@ -25,24 +24,28 @@
 
 		// Update the countdown every second
 		timerInterval = setInterval(updateCountdown, 1000);
-
-		// Ensure timer is cleared when component is destroyed
-		onDestroy(() => {
-			clearInterval(timerInterval);
-		});
 	});
 </script>
 
 <div class="flex flex-col justify-center items-center h-full">
 	<div class="flex flex-col justify-center items-center">
 		<div class="h-full flex items-center justify-center">
-			<img class="h-14 mr-3" src={logo} alt="Logo" />
-			<h1 class="text-white text-4xl pt-1 lg:pt-2 lg:text-6xl lg:pb-2">
+			<img class="h-16 mr-3" src={logo} alt="Logo" />
+			<h1 class="text-white text-5xl pt-1 lg:pt-2 lg:text-7xl lg:pb-2">
 				<span class="font-semibold font-sans">BITS</span><span class="font-cursive">megle</span>
 			</h1>
 		</div>
-		<div class="text-gray-400 text-md font-serif mt-4 mb-6">Kabhi online intro diya hai kya?</div>
+		<div class="text-gray-400 text-3xl font-serif mt-4 mb-6 tagline">
+			Kabhi online intro diya hai kya?
+		</div>
 		<div class="text-white text-xl font-serif m-2">Launching soon...</div>
 		<div class="text-white text-xl font-serif m-2">{countdown}</div>
 	</div>
 </div>
+
+<style>
+	.tagline {
+		font-family: 'Reenie Beanie', cursive;
+		font-weight: 400;
+	}
+</style>
