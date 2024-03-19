94f925505b19bee568d376557b70e831a0d5bc89
Swas.py <codewithswastik@gmail.com>
2024-03-19 13:17:55 +0530

    feat: Leaderboard sort

--- src/routes/leaderboard/+page.server.ts
+++ src/routes/leaderboard/+page.server.ts
@@ -2,8 +2,7 @@ import { users } from '../../db/users';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
-	const data = await users.find({}, { limit: 10 }).toArray();
-	console.log('data', data);
+	const data = await users.find({}).sort({ reputation: -1 }).limit(10).toArray();
 
 	const serializableData = data.map((item) => ({
 		...item,
