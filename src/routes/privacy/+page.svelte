d293e5d18cbf71181746b7052ab893d78bc0c075
Swas.py <codewithswastik@gmail.com>
2024-03-19 21:56:31 +0530

    feat: Start privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,176 @@
+<div class="bg-gray-900 text-white p-8 m-8 text-wrap rounded-lg">
+	<h1 class="text-3xl mb-6 font-bold">Privacy Policy</h1>
+	<div class="mb-2">
+		We are committed to safeguarding the privacy of our service users. This policy applies where we
+		are acting as a data controller with respect to the personal data of our service users; in other
+		words, where we determine the purposes and means of the processing of that personal data. Your
+		use of our service manifests your consent to the transmission of personal data specified in this
+		policy.
+	</div>
+	<h2 class="text-2xl mb-4 font-semibold">Use of Personal Info</h2>
+	<div class="mb-2">
+		We don't sell your personal data to advertisers, and we don't share information that directly
+		identifies you (such as your name, email address, or other contact information) with advertisers
+		unless you give us specific permission. Instead, advertisers can tell us things such as the kind
+		of audience that they want to see their ads, and we show those ads to people who may be
+		interested. We provide advertisers with reports about the performance of their ads that help
+		them understand how people are interacting with their content.
+	</div>
+	<div class="mb-2">
+		This means that we can show you relevant and useful ads without telling advertisers who you are.
+		We don't sell your personal data. We allow advertisers to tell us things such as their business
+		goal, and the kind of audience that they want to see their ads. We then show their ad to people
+		who we think might be interested. We also provide advertisers with reports about the performance
+		of their ads to help them understand how people are interacting with their content on and off
+		BITSmegle. We collect and use your personal data in order to provide the services described
+		above for you. You can learn about how we collect and use your data in our Privacy Policy.
+	</div>
+	<div class="mb-2">
+		To improve our Product and cater to your needs, we collect and use various information. This
+		includes storing your BITS mail ID and profile picture, tracking your interactions with features
+		like "reputation" and "leaderboard," and any new features introduced by BITSmegle in the future.
+	</div>
+	<div class="mb-2">
+		<ul class="list-disc">
+			<li>How we use information to improve our services:</li>
+			<li>See if a product is working correctly.</li>
+			<li>Troubleshoot and fix it when it's not</li>
+			<li>Try out new products and services to see if they work.</li>
+			<li>Get feedback on our ideas for products or features.</li>
+			<li>
+				Conduct surveys and other research about what you like about our Products and services, and
+				what we can do better.
+			</li>
+		</ul>
+	</div>
+
+	<div class="mb-2">
+		<ul class="list-disc">
+			<li>Promoting safety, security and integrity:</li>
+			<li>
+				None of the methods outlined below retain your video chat data, and we do not engage in
+				monitoring them. Any mention of information pertains solely to the data collected through
+				report buttons and your interactions with the "Reputation" feature.
+			</li>
+			<li>Here are some ways that we promote safety, security and integrity. We work to:</li>
+			<li>Verify accounts and activity</li>
+			<li>
+				Detect and rectify breaches of our terms or policies. In certain instances, determinations
+				regarding breaches are evaluated by the BITSmegle review committee, comprised of members
+				from varied demographics. They may consider the information available to us during their
+				assessment of our decisions.
+			</li>
+			<li>Investigate suspicious activity</li>
+			<li>Detect and prevent spam and other bad experiences</li>
+			<li>Maintain the integrity of our Products and services.</li>
+		</ul>
+	</div>
+
+	<div class="mb-2">
+		<p>
+			Providing measurement, analytics, and business services: To provide measurement and analytics
+			services, we use information like your interactions with the basic functionality of the
+			website, with ads (if provided in the future), and the time you spend on it. <strong
+				>We do not sell your personal data to advertisers.</strong
+			>
+		</p>
+	</div>
+
+	<div class="mb-2">
+		<p>
+			People and accounts that you share and communicate with: When you interact with people or
+			businesses, they can see:
+		</p>
+		<ul class="list-disc">
+			<li>What you share with them</li>
+			<li>Your video and audio feed</li>
+			<li>Your BITS e-mail id</li>
+			<li>Your Registered Name</li>
+		</ul>
+	</div>
+
+	<div class="mb-2">
+		<p>What we do not share with anyone:</p>
+		<ul class="list-disc">
+			<li>Your interactions with features like "Reputation" and "Report"</li>
+			<li>Your activity on site</li>
+		</ul>
+	</div>
+
+	<div class="mb-2">
+		<p>
+			How can you manage or delete your information and exercise your rights? Currently, we do not
+			offer the capability to delete your accounts and associated information. However, we are
+			actively pursuing this feature for implementation in upcoming updates.
+		</p>
+	</div>
+
+	<div class="mb-2">
+		<p>
+			To assert your rights, please contact us via email at bitsmegle@gmail.com, and we assure you a
+			response within 48 hours.
+		</p>
+	</div>
+
+	<pre>
+        What are cookies and what does this policy cover?
+
+Cookies are small pieces of text used to store information in web browsers. Cookies are used to store and receive identifiers and other information on computers, phones and other devices. Other technologies, including data that we store on your web browser or device, identifiers associated with your device and other software, are used for similar purposes. In this policy, we refer to all of these technologies as "cookies".
+
+
+Authentication
+We use cookies to verify your account and determine when you're logged in so that we can make it easier for you to access the BITSmegle.live and show you the appropriate experience and features.
+For example, We use cookies to keep you logged in as you navigate between BITSmegle pages. Cookies also help us remember your browser so you don't have to keep logging in to BITSmegle.live and so you can more easily log in to BITSmegle.live. 
+
+Security, site and product integrity
+We use cookies to help us keep your account, data and the BITSmegle safe and secure.
+We also use cookies to combat activity that violates our policies or otherwise degrades our ability to provide the BITSmegle.
+Cookies also help us prevent non-verified users from registering for BITSmegle accounts.
+
+Analytics and research
+We use cookies to better understand how people use the BTISmegle so that we can improve them.
+For example: Cookies can help us understand how people use the BITSmegle service, analyze which parts of our Products people find most useful and engaging, and identify features that could be improved.
+    </pre>
+	<pre>
+        **In terms of data storage and personalized ad delivery:**
+We refrain from gathering your video and audio streams from your interactions with random users. Consequently, your conversations remain unmonitored, relieving BITSmegle of responsibility for any inappropriate or harassing conduct.
+Users are encouraged to conduct themselves respectfully towards others on the platform, as their name and institute email ID will be visible to other individuals.
+
+Your activity and information that you provide:
+1. When you sign in to BITSmegle with your BITS email account, you implicitly consent to Google's Terms and Conditions, which include granting access to your device's webcam and microphone.
+2. Your IP address.
+3. Your interactions with inherent website features such as reputation, badges, and leaderboard.
+4. Your interaction with ads on websites (if any).
+The time, frequency, and duration of your activities on BITSmegle.
+
+The browser, and device information:
+We collect and receive information about the different devices you use and how you use them.
+
+Device information we collect and receive includes:
+
+Identifiers that tell your device apart from other users' devices, including device IDs.
+Signals from your device.
+Information you've shared with us through device settings, camera access, microphone access and 
+Information about the network that you connect your device to and your connection, including your.
+Information about our products' performance on your device.
+Information from cookies and similar technologies. 
+
+
+
+
+What if you don't let us collect certain information?
+Some information is required for our products to work. Other information is optional, but without it, the quality of your experience might be affected
+
+
+
+How long do we keep your information for?
+
+We keep information for as long as we need it to provide our services, comply with legal obligations, or protect our or other's interests. We decide how long we need information on a case-by-case basis. Here's what we consider when we decide:
+
+1. If we need it to operate or provide our products. For example, we need to keep some of your information to maintain your account.
+2. The feature that we use it for and how that feature works. For example, we might need your data regarding reputations and badges for better functioning of the website.
+3. If we need it for other legitimate purposes, such as to prevent harm; investigate possible violations of our terms or policies; promote safety, security and integrity; or protect ourselves, including our rights, property or services.
+
+
+    </pre>
+</div>
